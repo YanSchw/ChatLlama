327bcb3325bd1851b4615b930a3ae38254af5fa2
Yannick Schössow <102901459+YanSchw@users.noreply.github.com>
2024-06-20 11:58:07 +0200

    Import Initial Java Backend

---
+++ src/main/java/com/example/demo/RootController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public Resource index() {
+        return new ClassPathResource("html/index.html");
+    }
+
+    @GetMapping("/css/{style}")
+    public Resource css(@PathVariable String style) {
+        return new ClassPathResource("css/" + style);
+    }
+
+    @GetMapping(value = "/js/{script}", produces = "application/javascript")
+    public Resource js(@PathVariable String script) {
+        return new ClassPathResource("js/" + script);
+    }
+
+}
