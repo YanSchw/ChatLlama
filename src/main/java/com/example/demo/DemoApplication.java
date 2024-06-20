19d0e9ba24021800fb5dbe56c9f3a7ec61c225d8
Yannick Schössow <102901459+YanSchw@users.noreply.github.com>
2024-06-20 13:54:34 +0200

    Added Ollama4J

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import io.github.amithkoujalgi.ollama4j.core.OllamaAPI;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initApplication() {
+        String host = "http://host.docker.internal:11434/";
+
+        try {
+            OllamaAPI ollamaAPI = new OllamaAPI(host);
+            ollamaAPI.setVerbose(true);
+            boolean isOllamaServerReachable = ollamaAPI.ping();
+            LoggerFactory.getLogger(DemoApplication.class).info("Is Ollama server alive: " + isOllamaServerReachable);
+        } catch (Exception e) {
+            LoggerFactory.getLogger(DemoApplication.class).error("Ollama server is unavailable!");
+            throw new RuntimeException();
+        }
     }
 
 }
