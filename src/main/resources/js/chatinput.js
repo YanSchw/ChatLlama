c526f58dc8aff2027a5128f3e12f0d60123bfe83
Yannick Schössow <102901459+YanSchw@users.noreply.github.com>
2024-06-26 12:49:43 +0200

    Updated Frontend + Styling

--- src/main/resources/js/chatinput.js
+++ src/main/resources/js/chatinput.js
@@ -10,16 +10,20 @@ function prompt(value) {
 }
 
 component('chat-input', (node, state) => {
-    createNode('div', node, div => {
-        createNode('input', div, input => {
-            input.setAttribute('type', 'text');
-            input.setAttribute('placeholder', 'Enter your prompt here');
-            input.onkeydown = function(e) {
-                if(e.keyCode == 13){
-                    prompt(input.value);
-                    input.value = '';
-                }
-             };
-        });
+    createNode('span', node, span => {
+        span.innerHTML = '&#x1F999';
+    });
+    createNode('input', node, input => {
+        input.setAttribute('type', 'text');
+        input.setAttribute('placeholder', 'Ask ChatLlama anything...');
+        input.onkeydown = function(e) {
+            if(e.keyCode == 13){
+                prompt(input.value);
+                input.value = '';
+            }
+         };
+    });
+    createNode('span', node, span => {
+        span.innerHTML = '&#9999;';
     });
 });
