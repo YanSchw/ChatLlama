c3940a0d35038332a8c2a0acc0656987f94e9390
Yannick Schössow <102901459+YanSchw@users.noreply.github.com>
2024-06-30 02:42:56 +0200

    Added Side-Bar

--- src/main/resources/js/header.js
+++ src/main/resources/js/header.js
@@ -4,7 +4,7 @@
 component('header', (node, state) => {
     createNode('span', node, span => {
         span.addEventListener("click", () => {
-            alert('hi');
+            select("side-bar").classList.toggle("active");
         });
         span.innerHTML = '&#128218;';
     });
