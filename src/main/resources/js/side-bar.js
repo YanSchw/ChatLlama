5ff8b8eb1b705c70abef6aba6427b753d50856d5
Yannick Schössow <102901459+YanSchw@users.noreply.github.com>
2024-07-03 00:56:39 +0200

    Added chat selection

---
+++ src/main/resources/js/side-bar.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function fetchAllChats() {
+    
+    fetch(`/api/allChats`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => response.json())
+      .then(json => {
+        let container = select('div.chats-container');
+        container.innerHTML = '';
+        for (let chat of json.chats) {
+            createNode('nav-chat', container, nav => {
+                nav.innerHTML = chat.title;
+                nav.addEventListener("click", () => {
+                    fetchChatMessages(chat.chatid);
+                    window.scrollTo(0, 0);
+                });
+            });
+        }
+
+    });
+}
+
+fetchAllChats();
