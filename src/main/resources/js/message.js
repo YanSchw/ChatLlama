65756d909141876ec133c31162ee459868013245
Yannick Schössow <102901459+YanSchw@users.noreply.github.com>
2024-07-31 17:11:23 +0200

    Added Markdown processing

--- src/main/resources/js/message.js
+++ src/main/resources/js/message.js
@@ -1,9 +1,23 @@
 "use strict";
 
+function processMarkdown(markdown) {
+    // Convert newlines to <br>
+    let html = markdown.replace(/\n/g, '<br>');
+
+    // Convert **bold** to <b>bold</b>
+    html = html.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
+
+    // Convert _italic_ and *italic* to <i>italic</i>
+    html = html.replace(/_(.*?)_/g, '<i>$1</i>');
+    html = html.replace(/\*(.*?)\*/g, '<i>$1</i>');
+
+    return html;
+}
+
 component('message', (node, state) => {
     createNode('div', node, div => {
         createNode('p', div, p => {
-            p.innerText = state.msg;
+            p.innerHTML = processMarkdown(state.msg);
         });
     });
 });
