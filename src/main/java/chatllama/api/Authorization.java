28476eb5a1573f3ad16af7d9e8c93d5716ee4c37
Yannick Schössow <102901459+YanSchw@users.noreply.github.com>
2024-08-01 23:42:08 +0200

    Update Authorization.java

--- src/main/java/chatllama/api/Authorization.java
+++ src/main/java/chatllama/api/Authorization.java
@@ -2,6 +2,7 @@ package chatllama.api;
 
 import chatllama.data.entity.SessionToken;
 import chatllama.data.service.SessionTokenService;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,11 @@ public class Authorization {
     @GetMapping(value = "/auth/session/isvalid", produces = "application/json")
     public String isvalid(@RequestHeader("Session-Token") String token) {
         SessionToken sessionToken = SessionTokenService.getInstance().getByToken(token);
-        return sessionToken != null ? "true" : "false";
+
+        JSONObject json = new JSONObject();
+        json.put("isvalid", sessionToken != null);
+
+        return json.toString();
     }
 
 }
