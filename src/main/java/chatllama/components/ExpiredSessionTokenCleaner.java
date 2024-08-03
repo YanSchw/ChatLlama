119a05e302fe18330182f1742d4deedf8c126dc3
Yannick Schössow <102901459+YanSchw@users.noreply.github.com>
2024-08-03 15:59:21 +0200

    Reformat code

--- src/main/java/chatllama/components/ExpiredSessionTokenCleaner.java
+++ src/main/java/chatllama/components/ExpiredSessionTokenCleaner.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
